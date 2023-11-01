3e9a898e84a9699307d78177243652e7c4cc94f8
Katya Kinyapina <kinyapina.katya28@gmail.com>
2023-11-01 20:59:35 +0700

    Четвертое задание выполнено полностью

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,37 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (this.hasOwnProperty('excluded')) {
+    return 0;
+  }
+
+  if (marks !== undefined || this.marks.length !== 0) {
+    this.marks.push(...marks);
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (!this.hasOwnProperty('marks') || !this.marks || this.marks.length === 0) {
+    return 0;
+  }
+
+  let average = 0;
+
+  return average = this.marks.reduce((sum, item) => sum + item, 0) / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+
+  this.excluded = reason;
 }
